a09b25ecf1ad29eb3d14c86d3859382abe9b1a3d
Napat <karns.napat@gmail.com>
2021-03-10 02:10:24 -0800

    change * to / for index.html

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
 
 app.use(express.static('./client/build/'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname+'/client/build/index.html');
 });
 
