51239ba5a30f74d4b7795fe9f312dce8c4acf6e6
Napat <karns.napat@gmail.com>
2021-03-10 01:11:16 -0800

    added morgan import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
-const path = require('path');
+var logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 
